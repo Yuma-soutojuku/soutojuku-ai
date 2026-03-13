4cd7010ab1143de611e4d7ac49e5997e00faab7e
Yuma K <s20052@tokyo.kosen-ac.jp>
2026-03-13 15:16:54 +0900

    キャッシュ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { client } from "../libs/microcms";
 import TopPage from "../components/TopPage";
 
+export const revalidate = 0;
+
 export default async function Home() {
   let news = [];
   let courses = [];
@@ -12,8 +14,8 @@ export default async function Home() {
     // 3つのAPIから並列でデータを取得
     const [newsData, coursesData, instructorsData,activeClassesData,pastClassesData] = await Promise.all([
       client.get({ endpoint: "news" }),
-      client.get({ endpoint: "courses" }),
-      client.get({ endpoint: "instructors" }),
+      client.get({ endpoint: "courses", queries: { limit: 100 }}),
+      client.get({ endpoint: "instructors", queries: { limit: 100 }}),
       client.get({ endpoint: "active_classes", queries: { limit: 100 }}),
       client.get({ endpoint: "past_classes" , queries: { limit: 100 }}),
     ]);
